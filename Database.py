0cc4cd5fe13babf9080f311357913302d9328b89
faddiDev <faddi.dev@gmail.com>
2020-10-25 10:37:55 +0700

    Python Application CLI

--- Database.py
+++ Database.py
@@ -1,10 +1,6 @@
-####################################################################################
-#Author    : Faddi Susanto  													   #
-#Important : You can edit or share this application but do                         #
-#            not do commercial as the author are not take credit from it           #
-#Penting   : Silahkan meng-edit atau membagikan aplikasi ini namun dilarang        #
-#            keras melakukan penjualan karena pencipta aplikasi tidak meminta uang #
-####################################################################################
+###############################################
+# Author    : Faddi Susanto                   #
+###############################################
 
 class Database:
     
